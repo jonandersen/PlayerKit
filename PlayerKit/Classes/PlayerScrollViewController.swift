08e06611dd81d0ee8298366ed68e49a0cc653e47
Jon Andersen <jon.andersen.se@gmail.com>
2020-07-12 01:25:28 -0400

    [Fix] Mix with others

--- PlayerKit/Classes/PlayerScrollViewController.swift
+++ PlayerKit/Classes/PlayerScrollViewController.swift
@@ -76,7 +76,7 @@ public class PlayerScrollViewController: UIViewController {
     override public func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
         do {
-            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+			try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default, options: .mixWithOthers)
             try AVAudioSession.sharedInstance().setActive(true)
         } catch {
             // Didn't work
